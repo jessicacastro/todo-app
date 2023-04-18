6f8fed13d01cef26b098d8c83b0a65b77352bb7c
Jessica Castro <Jessica.castros@outlook.com>
2023-04-18 14:34:11 -0300

    Add new task functionality on component

--- src/components/NewTask/NewTask.tsx
+++ src/components/NewTask/NewTask.tsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import { PlusCircle } from 'phosphor-react'
+
 import styles from './NewTask.module.css'
 
-interface NewTaskProps extends React.InputHTMLAttributes<HTMLInputElement>{}
+interface NewTaskProps extends React.InputHTMLAttributes<HTMLInputElement>{
+  onCreateNewTask: (title: string) => void
+}
 
-export const NewTask = ({}: NewTaskProps) => {
+export const NewTask = ({onCreateNewTask}: NewTaskProps) => {
+  const [title, setTitle] = React.useState<string>('')
+
+  const handleCreateTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    
+    onCreateNewTask(title)
+    setTitle('')
+  }
+
+  const handleNewTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+    event.target.setCustomValidity('')
+  }
+
+  const handleNewTitleInvalid = (event: React.InvalidEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    event.target.setCustomValidity('Digite um título para a tarefa')
+  }
+
   return(
-    <form className={styles['form-new-task']} action="">
-      <input className={styles.input} type="text" placeholder="Adicione uma nova tarefa" />
-      <button className={styles['create-button']} type="submit">Criar <PlusCircle /></button>
+    <form onSubmit={handleCreateTask} className={styles.formNewTask} action="">
+      <input 
+        onChange={handleNewTitleChange}
+        onInvalid={handleNewTitleInvalid}
+        value={title}
+        className={styles.input} 
+        type="text" 
+        placeholder="Adicione uma nova tarefa" />
+      <button className={styles.createButton} type="submit">Criar <PlusCircle /></button>
     </form>
   )
 }
